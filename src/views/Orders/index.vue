f2fb8acadfba4b6301e0398e5281caded148ef4a
Saul Espinoza <saul_zeul@hotmail.com>
2019-06-26 17:07:19 -0500

    route view products and props

--- src/views/Orders/index.vue
+++ src/views/Orders/index.vue
@@ -22,25 +22,17 @@
           </el-col>
         </el-row>
         <el-row :gutter="10">
-          <el-col :xs="12" :sm="6" :md="6" :lg="6" :xl="3">
-            <Product :nameItem="nameItem" :urlImage="urlImage"/>
-          </el-col>                      
+          <el-col :span="24">
+            <router-view></router-view>
+          </el-col>
         </el-row>
       </el-col>
     </el-row>
   </div>
 </template>
 <script>
-import Product from "@/components/Global/Product";
 export default {
-  components: { Product },
   name: "Orders",
-  data() {
-    return {
-      nameItem: 'Hamburguesa sencilla',
-      urlImage: 'https://via.placeholder.com/150/24f355'
-    }
-  },
 };
 </script>
 <style lang="scss" scoped>
--- src/views/Orders/products.vue
+++ src/views/Orders/products.vue
@@ -1,5 +1,33 @@
 <template>
-    <div>
-        <h2>Order</h2>
-    </div>
+  <div>
+    <el-row :gutter="10">
+      <el-col :xs="12" :sm="6" :md="6" :lg="6" :xl="3">
+        <Product :nameItem="nameItem" :urlImage="urlImage"/>
+      </el-col>
+      <el-col :xs="12" :sm="6" :md="6" :lg="6" :xl="3">
+        <Product :nameItem="nameItem" :urlImage="urlImage"/>
+      </el-col>
+      <el-col :xs="12" :sm="6" :md="6" :lg="6" :xl="3">
+        <Product :nameItem="nameItem" :urlImage="urlImage"/>
+      </el-col>
+      <el-col :xs="12" :sm="6" :md="6" :lg="6" :xl="3">
+        <Product :nameItem="nameItem" :urlImage="urlImage"/>
+      </el-col>
+      <el-col :xs="12" :sm="6" :md="6" :lg="6" :xl="3">
+        <Product :nameItem="nameItem" :urlImage="urlImage"/>
+      </el-col>
+    </el-row>
+  </div>
 </template>
+<script>
+import Product from "@/components/Global/Product";
+export default {
+  components: { Product },
+  data() {
+    return {
+      nameItem: "Hamburguesa sencilla",
+      urlImage: "https://via.placeholder.com/150/24f355"
+    };
+  }
+};
+</script>
